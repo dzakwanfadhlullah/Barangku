31ba39de2e6f91331e7893ba8a839cff68e60547
Dzakwan Fadhlullah <dzakwan22001@mail.unpad.ac.id>
2026-04-10 20:16:14 +0700

    feat(core): add reusable AppTextField widget

---
+++ app/lib/core/widgets/app_text_field.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import '../../theme/app_colors.dart';
+import '../../theme/app_text_styles.dart';
+
+class AppTextField extends StatefulWidget {
+  final String label;
+  final String? hintText;
+  final TextEditingController? controller;
+  final bool isPassword;
+  final TextInputType keyboardType;
+  final String? helperText;
+
+  const AppTextField({
+    super.key,
+    required this.label,
+    this.hintText,
+    this.controller,
+    this.isPassword = false,
+    this.keyboardType = TextInputType.text,
+    this.helperText,
+  });
+
+  @override
+  AppTextFieldState createState() => AppTextFieldState();
+}
+
+class AppTextFieldState extends State<AppTextField> {
+  bool _obscureText = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _obscureText = widget.isPassword;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          widget.label,
+          style: AppTextStyles.labelMd.copyWith(
+            color: AppColors.softCharcoal,
+          ),
+        ),
+        const SizedBox(height: 8),
+        TextField(
+          controller: widget.controller,
+          obscureText: _obscureText,
+          keyboardType: widget.keyboardType,
+          style: AppTextStyles.bodyLg,
+          decoration: InputDecoration(
+            hintText: widget.hintText,
+            helperText: widget.helperText,
+            helperMaxLines: 2,
+            helperStyle: AppTextStyles.bodySm.copyWith(
+              color: AppColors.neutralDivider.withOpacity(0.9), // subtle helper text
+            ),
+            suffixIcon: widget.isPassword
+                ? IconButton(
+                    icon: Icon(
+                      _obscureText ? Icons.visibility_off_outlined : Icons.visibility_outlined,
+                      color: AppColors.warmMutedGray,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _obscureText = !_obscureText;
+                      });
+                    },
+                  )
+                : null,
+          ),
+        ),
+      ],
+    );
+  }
+}
